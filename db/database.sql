5b72f499f4b5d98e29e5f95e679aea086784127f
dowran21 <ddowran2106@gmail.com>
2021-10-20 17:30:24 +0500

    delelted descriptions

--- db/database.sql
+++ db/database.sql
@@ -45,8 +45,7 @@ CREATE TABLE products(
     new_in_come BOOLEAN DEFAULT FALSE,
     quantity INTEGER NOT NULL,
     deleted BOOLEAN DEFAULT FALSE,
-    description_tm TEXT NOT NULL,
-    description_ru TEXT NOT NULL,
+    composition VARCHAR(300),
     UNIQUE(product_name),
 
     CONSTRAINT producer_id_fk FOREIGN KEY (producer_id) REFERENCES producers(id)
