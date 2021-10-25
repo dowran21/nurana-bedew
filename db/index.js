f58b16842047c37c9083fa950cec913d345efc9a
dowran21 <ddowran2106@gamil.com>
2021-10-25 19:30:33 +0500

    db/index js

--- db/index.js
+++ db/index.js
@@ -3,10 +3,10 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool(
 {
-  user: process.env.DB_USER || 'dowran',
+  user: process.env.DB_USER || 'guych',
   host: process.env.DB_HOST || 'localhost',
   database: process.env.DB_NAME || 'nurly_bedew',
-  password: process.env.DB_PASSWORD || '61123141dow',
+  password: process.env.DB_PASSWORD || 'gp221090gp',
   port: process.env.DB_PORT || 5432
 });
 
