6e6b59f1c0c616158b292576c6b9beb243ddc757
dowran21 <ddowran2106@gamil.com>
2021-11-09 15:03:57 +0500

    fixed search in back

--- controller/AdminController.js
+++ controller/AdminController.js
@@ -373,7 +373,7 @@ const GetAllProducts = async (req, res) =>{
     }
     let WherePart = ``
     if (product_name && product_name !== 'undefined'){
-        WherePart += ` AND (p.product_name ~* '${product_name}' OR p.description_tm ~* '${product_name}')`
+        WherePart += ` AND p.product_name ~* '${product_name}' `
     }
     if(producer_id && producer_id !== 'undefined'){
         WherePart += ` AND p.producer_id = ${producer_id}`
